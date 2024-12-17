28aa011c50ec5278273325b616a7cb7780f85f8e
Rudra Langote <langoterudra@gmail.com>
2024-12-17 14:21:31 +0530

    Completed Calulator

--- index.js
+++ index.js
@@ -23,7 +23,20 @@ function Call(){
     document.getElementById('eq').value = result
     str = " "
 
-
-
-
 }
+
+document.addEventListener('keydown', (e)=>{
+    if(["1","2","3","4","5","6","7","8","9","0","+", "-", "%", "/", "*", "(", ")", "Backspace", "Enter"].includes(e.key)){
+        if(e.key === "Backspace"){
+            Cut()
+        }
+        else if(e.key === "Enter"){
+            e.preventDefault()
+            Call()
+        }
+        else{
+            str = str + e.key
+            document.getElementById('eq').value = str
+        }
+    }
+})
